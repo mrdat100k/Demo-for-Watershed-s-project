4e4a6784ad9d9dcf20ba494029b0c2948d9ba41e
HUNGLiquid <32360741+HUNGLiquid@users.noreply.github.com>
2017-09-28 23:15:26 +0700

    Update main.cpp

--- CODE/src/main.cpp
+++ CODE/src/main.cpp
@@ -11,30 +11,32 @@ DigitalOut myled(LED1);
 extern InterruptIn btn_Set;
 extern InterruptIn btn_Select;
 DigitalOut relay(InverterEnable);
-INA219 sensor1(I2C_SDA,I2C_SCL);
-INA219 sensor2(I2C_SDA,I2C_SCL,0x41);
+INA219 sensor1(I2C_SDA, I2C_SCL);
+INA219 sensor2(I2C_SDA, I2C_SCL, 0x41);
 extern uint8_t mode;
 extern bool timerOn1;
 extern bool timerOn2;
 uint8_t sec_, min_, hour_;
 extern Adafruit_SSD1306_I2c gOled;
+
 void getDataINA(INA219* sensor, float* volt, float* curr, float* power)
 {
-  *volt=sensor->read_bus_voltage();
-  *curr=sensor->read_current_mA();
-  *power=sensor->read_power_mW();
+    *volt=sensor->read_bus_voltage();
+    *curr=sensor->read_current_mA();
+    *power=sensor->read_power_mW();
 }
 
-int main() {
+int main() 
+{
     float pvVolt, pvCurr, pvPower, pvEnergy, batVolt, batCurr, batPower, batEnergy;
     // put your setup code here, to run once:
     relay.write(0);
     wait_ms(300);
     gOled.begin();
     gOled.clearDisplay();
-    gOled.setTextCursor(0,0);
+    gOled.setTextCursor(0, 0);
     gOled.drawBitmap(0, 6, watershedlogo, 128, 48, WHITE);
-    gOled.setTextCursor(0,0);
+    gOled.setTextCursor(0, 0);
     gOled.display();
     sensor1.calibrate_32v_3200A();
     sensor2.calibrate_32v_3200A();
@@ -48,22 +50,42 @@ int main() {
     wait(2);
     set_time(0);
 
-    while(1) {
+    while(1)
+    {
         // put your main code here, to run repeatedly:
         uint32_t seconds = time(NULL);
-        sec_=seconds%60;
-        min_=seconds%3600/60;
-        hour_=seconds%86400/3600;
-        getDataINA(&sensor1,&batVolt,&batCurr,&batPower);
-        getDataINA(&sensor2,&pvVolt,&pvCurr,&pvPower);
-        batEnergy=batPower/1000*(float)seconds/3600;
-        pvEnergy=pvPower/1000*(float)seconds/3600;
-        if(mode==0) menu1_display(true,batVolt/1000,true,true);
-        if(mode==1) menu2_display(pvVolt/1000,pvCurr,pvPower/1000,pvEnergy,timerOn1,hour_,min_,sec_);
-        if(mode==2) menu3_display(batVolt/1000,batCurr,batPower/1000,batEnergy,timerOn2,hour_,min_,sec_);
-        if(batCurr<50) relay.write(1);
-        else relay.write(0);
-        myled=!myled;
+        sec_ = seconds % 60;
+        min_ = seconds % 3600 / 60;
+        hour_ = seconds % 86400 / 3600;
+        getDataINA(&sensor1, &batVolt, &batCurr,  &batPower);
+        getDataINA(&sensor, &pvVolt, &pvCurr, &pvPower);
+        batEnergy = batPower / 1000  * (float)seconds / 3600;
+        pvEnergy = pvPower / 1000 * (float)seconds / 3600;
+        if(mode == 0)
+        {
+            menu1_display(true, batVolt / 1000, true, true);
+        }
+     
+        if(mode == 1)
+        {
+           menu2_display(pvVolt / 1000, pvCurr, pvPower / 1000, pvEnergy, timerOn1, hour_, min_, sec_);
+        }
+     
+        if(mode == 2)
+        {
+           menu3_display(batVolt /1000, batCurr, batPower / 1000, batEnergy, timerOn2, hour_, min_, sec_);
+        }
+     
+        if(batCurr < 50)
+        {
+           relay.write(1);
+        }
+        else
+        {
+           relay.write(0);
+        }
+     
+        myled =! myled;
         wait_ms(100);
     }
 }
