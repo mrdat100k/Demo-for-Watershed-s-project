0ceee66fb0bd2924ba7d0ff6e91276e48a672520
HUNGLiquid <32360741+HUNGLiquid@users.noreply.github.com>
2017-09-28 22:33:14 +0700

    Update interruptfunc.h

--- CODE/lib/IO/interruptfunc.h
+++ CODE/lib/IO/interruptfunc.h
@@ -11,33 +11,66 @@ bool timerOn1=true;
 bool timerOn2=true;
 void attimeout()
 {
-  mode=0;
+    mode=0;
 }
+
 void btn_select()
 {
-  btn_Select.disable_irq();
-  timeout.attach(&attimeout,15);
-  wait_ms(20);
-  mode++;
-  if(mode>=3) mode=0;
-  btn_Select.enable_irq();
+    btn_Select.disable_irq();
+    timeout.attach(&attimeout,15);
+    wait_ms(20);
+    mode++;
+    if(mode >= 3) 
+    {
+        mode=0;
+    }
+  
+    btn_Select.enable_irq();
 }
+
 void btn_set()
 {
-  btn_Set.disable_irq();
-  timeout.attach(&attimeout,15);
-  wait_ms(20);
-  if(mode==1) {timerOn1=!timerOn1; time1=time(NULL);}
-  if(mode==2) {timerOn2=!timerOn2; time1=time(NULL);}
-  btn_Set.enable_irq();
+    btn_Set.disable_irq();
+    timeout.attach(&attimeout,15);
+    wait_ms(20);
+    if(mode == 1)
+    {
+        timerOn1=!timerOn1; 
+        time1=time(NULL);
+    }
+  
+    if(mode == 2) 
+    { 
+        timerOn2=!timerOn2; 
+        time1=time(NULL);
+    }
+  
+    btn_Set.enable_irq();
 }
+
 void btn_press()
 {
-  btn_Set.disable_irq();
-  timeout.attach(&attimeout,15);
-  wait_ms(20);
-  if(mode==1||mode==2) time2=time(NULL);
-  if((time2-time1)>=2)   {set_time(0); if(mode==1) timerOn1=!timerOn1; if(mode==2) timerOn2=!timerOn2;}
-  btn_Set.enable_irq();
+    btn_Set.disable_irq();
+    timeout.attach(&attimeout,15);
+    wait_ms(20);
+    if((mode == 1) || (mode == 2))
+    {
+        time2=time(NULL);
+    }
+  
+    if((time2-time1) >= 2)
+    {
+        set_time(0); 
+        if(mode == 1) 
+        {
+            timerOn1 =! timerOn1; 
+            if(mode == 2) 
+            {
+              timerOn2 =! timerOn2;
+            }
+        }
+    }
+  
+    btn_Set.enable_irq();
 }
 #endif
