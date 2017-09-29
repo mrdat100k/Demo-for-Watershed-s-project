b8584e55069af354f95ae2229172e95c5ec1b34f
Nguyễn Bá Đạt <32257724+mrdat100k@users.noreply.github.com>
2017-09-29 11:15:36 +0700

    Update interruptfunc.h

--- CODE/lib/IO/interruptfunc.h
+++ CODE/lib/IO/interruptfunc.h
@@ -1,7 +1,24 @@
+/** @file iopin.h
+*
+* @brief This file include interrupt service routine for buttons.
+* 
+* @par
+* COPYRIGHT NOTICE: (c) 2017 MANDEVICES GROUP, HUST.
+*/
 #ifndef _INTFUNC_H
 #define _INTFUNC_H
+
+#ifdef __cplusplus
+extern “C” {
+#endif
+/******************************************************************************
+INCLUDES
+******************************************************************************/
 #include<mbed.h>
 #include<iopin.h>
+/******************************************************************************
+GLOBAL VARIABLES
+******************************************************************************/
 uint8_t g_mode=0;
 Timeout g_time_out_s;
 time_t g_time_1_s; //chuc nang ?
@@ -10,12 +27,42 @@ InterruptIn g_setting_button(set);
 InterruptIn g_selecting_button(select);
 bool g_timer_on_1_=true; //
 bool  g_timer_on_2_=true;//
+/******************************************************************************
+* DESCRIPTION: the timeout service routine, when timeout reset mode to switch back the main menu
+* @author:
+* Nguyen Ba Dat - Member of MANDEVICES GROUP
+* @version: v1.0.0
+* @param:
+* NONE
+* @return: NONE
+* @see:
+* .../lib/IO/interruptfunc.h
+* @todo:
+* NONE
+* @bug:
+* NONE
+******************************************************************************/
 void set_mode_when_timeout() 
 {
     g_mode=0;
 }
 
-void press_button_to_seclect()
+/******************************************************************************
+* DESCRIPTION: the Interrupt service routine of fall interrupt when press the select button
+* @author:
+* Nguyen Ba Dat - Member of MANDEVICES GROUP
+* @version: v1.0.0
+* @param:
+* NONE
+* @return: NONE
+* @see:
+* .../lib/IO/interruptfunc.h
+* @todo:
+* NONE
+* @bug:
+* NONE
+******************************************************************************/
+static void fall_select_btn_isr()
 {
     g_button_select.disable_irq();
     g_time_out_s.attach(&set_mode_when_timeout, 15);
@@ -28,8 +75,22 @@ void press_button_to_seclect()
   
     press_button.enable_irq();
 }
-
-void press_button_to_set() 
+/******************************************************************************
+* DESCRIPTION: the Interrupt service routine of fall interrupt when press the set button
+* @author:
+* Nguyen Ba Dat - Member of MANDEVICES GROUP
+* @version: v1.0.0
+* @param:
+* NONE
+* @return: NONE
+* @see:
+* .../lib/IO/interruptfunc.h
+* @todo:
+* NONE
+* @bug:
+* NONE
+******************************************************************************/
+static void fall_set_btn_isr() 
 {
     btn_Set.disable_irq();
     timeout.attach(&attimeout, 15);
@@ -48,8 +109,22 @@ void press_button_to_set()
   
     btn_Set.enable_irq();
 }
-
-void btn_press()
+/******************************************************************************
+* DESCRIPTION: the Interrupt service routine of rise interrupt when press the set button
+* @author:
+* Nguyen Ba Dat - Member of MANDEVICES GROUP
+* @version: v1.0.0
+* @param:
+* NONE
+* @return: NONE
+* @see:
+* .../lib/IO/interruptfunc.h
+* @todo:
+* NONE
+* @bug:
+* NONE
+******************************************************************************/
+static void rise_set_btn_isr()
 {
     btn_Set.disable_irq();
     timeout.attach(&attimeout, 15);
@@ -74,4 +149,4 @@ void btn_press()
   
     btn_Set.enable_irq();
 }
-#endif
+#endif /* _INTFUNC_H */
