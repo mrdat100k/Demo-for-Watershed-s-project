c884695c636855acc43231fa57cd2be2d4d6b599
tuangjv <32361023+tuangjv@users.noreply.github.com>
2017-09-29 07:28:02 +0700

    Update interruptfunc.h
    Đạt cmt chức năng từng hàm để a đặt tên

--- CODE/lib/IO/interruptfunc.h
+++ CODE/lib/IO/interruptfunc.h
@@ -2,33 +2,34 @@
 #define _INTFUNC_H
 #include<mbed.h>
 #include<iopin.h>
-uint8_t mode=0;
-Timeout timeout;
-time_t time1, time2;
-InterruptIn btn_Set(set);
-InterruptIn btn_Select(select);
-bool timerOn1=true;
-bool timerOn2=true;
-void attimeout()
+uint8_t g_mode=0;
+Timeout g_time_out_s;
+time_t g_time_1_s; //chuc nang ?
+time_t g_time_2_s; // chuc nang?
+InterruptIn g_button_set(set);
+InterruptIn g_button_select(select);
+bool g_timer_on_1_=true; //
+bool  g_timer_on_2_=true;//
+void set_mode_when_timeout() 
 {
-    mode=0;
+    g_mode=0;
 }
 
-void btn_select()
+void press_button_to_seclect()
 {
-    btn_Select.disable_irq();
-    timeout.attach(&attimeout, 15);
+    g_button_select.disable_irq();
+    g_time_out_s.attach(&set_mode_when_timeout, 15);
     wait_ms(20);
-    mode++;
-    if(mode >= 3) 
+    g_mode++;
+    if(g_mode >= 3) 
     {
-        mode=0;
+        g_mode=0;
     }
   
-    btn_Select.enable_irq();
+    press_button.enable_irq();
 }
 
-void btn_set()
+void press_button_to_set() 
 {
     btn_Set.disable_irq();
     timeout.attach(&attimeout, 15);
