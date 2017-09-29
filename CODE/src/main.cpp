fec0b16aa63f044bf65dea5a51a9f154ce26569f
tuangjv <32361023+tuangjv@users.noreply.github.com>
2017-09-29 07:38:29 +0700

    Update main.cpp

--- CODE/src/main.cpp
+++ CODE/src/main.cpp
@@ -8,18 +8,20 @@ int foo=0;
 #include <interruptfunc.h>
 #include <logo.h>
 DigitalOut myled(LED1);
-extern InterruptIn btn_Set;
-extern InterruptIn btn_Select;
+extern InterruptIn g_setting_button;
+extern InterruptIn g_selecting_button;
 DigitalOut relay(InverterEnable);
 INA219 sensor1(I2C_SDA, I2C_SCL);
 INA219 sensor2(I2C_SDA, I2C_SCL, 0x41);
-extern uint8_t mode;
-extern bool timerOn1;
-extern bool timerOn2;
-uint8_t sec_, min_, hour_;
+extern uint8_t g_mode;
+extern bool g_timer_on_1_s;
+extern bool g_timer_on_2_s;
+uint8_t g_sec_;
+uint8_t g_min_;
+uint8_t g_hour_;
 extern Adafruit_SSD1306_I2c gOled;
 
-void getDataINA(INA219* sensor, float* volt, float* curr, float* power)
+void get_data_ina(INA219* sensor, float* volt, float* curr, float* power)
 {
     *volt=sensor->read_bus_voltage();
     *curr=sensor->read_current_mA();
@@ -28,7 +30,14 @@ void getDataINA(INA219* sensor, float* volt, float* curr, float* power)
 
 int main() 
 {
-    float pvVolt, pvCurr, pvPower, pvEnergy, batVolt, batCurr, batPower, batEnergy;
+    float pvvolt;
+    float pvCurr;
+    float pvPower;
+    float pvEnergy;
+    float batVolt;
+    float batCurr;
+    float batPower;
+    float batEnergy;
     // put your setup code here, to run once:
     relay.write(0);
     wait_ms(300);
@@ -40,13 +49,13 @@ int main()
     gOled.display();
     sensor1.calibrate_32v_3200A();
     sensor2.calibrate_32v_3200A();
-    btn_Set.disable_irq();
-    btn_Select.disable_irq();
-    btn_Set.fall(&btn_set);
-    btn_Select.fall(&btn_select);
-    btn_Set.rise(&btn_press);
-    btn_Set.enable_irq();
-    btn_Select.enable_irq();
+    g_setting_button.disable_irq();
+    g_selecting_button.disable_irq();
+    g_setting_button.fall(&btn_set);
+    g_selecting_button.fall(&btn_select);
+    g_setting_button.rise(&btn_press);
+    g_setting_button.enable_irq();
+    g_selecting_button.enable_irq();
     wait(2);
     set_time(0);
 
@@ -54,24 +63,25 @@ int main()
     {
         // put your main code here, to run repeatedly:
         uint32_t seconds = time(NULL);
-        sec_ = seconds % 60;
-        min_ = seconds % 3600 / 60;
-        hour_ = seconds % 86400 / 3600;
-        getDataINA(&sensor1, &batVolt, &batCurr,  &batPower);
-        getDataINA(&sensor, &pvVolt, &pvCurr, &pvPower);
-        batEnergy = batPower / 1000  * (float)seconds / 3600;
+        g_sec_ = seconds % 60;
+        g_min_ = seconds % 3600 / 60;
+        g_hour_ = seconds % 86400 / 3600;
+        get_data_ina(&sensor1, &batVolt, &batCurr,  &batPower);
+        get_data_ina(&sensor, &pvVolt, &pvCurr, &pvPower);
+        
+       = batPower / 1000  * (float)seconds / 3600;
         pvEnergy = pvPower / 1000 * (float)seconds / 3600;
-        if(mode == 0)
+        if(g_mode == 0)
         {
             menu1_display(true, batVolt / 1000, true, true);
         }
      
-        if(mode == 1)
+        if(g_mode == 1)
         {
            menu2_display(pvVolt / 1000, pvCurr, pvPower / 1000, pvEnergy, timerOn1, hour_, min_, sec_);
         }
      
-        if(mode == 2)
+        if(g_mode == 2)
         {
            menu3_display(batVolt /1000, batCurr, batPower / 1000, batEnergy, timerOn2, hour_, min_, sec_);
         }
